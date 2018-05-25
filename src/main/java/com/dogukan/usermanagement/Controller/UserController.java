4e6cf328fbfcaa4d7b088aa214bf3aae99d4c5c4
Dogukan Yılmaz <dogukanyilmaz95@hotmail.com>
2018-05-25 14:00:22 +0300

    RequestParam --> RequestBody

--- src/main/java/com/dogukan/usermanagement/Controller/UserController.java
+++ src/main/java/com/dogukan/usermanagement/Controller/UserController.java
@@ -7,10 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Response;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
